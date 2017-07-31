1bb94fe20bc7edb086db706b1a2c40ced81a73d4
Shashank <abc@shanky.xyz>
2017-07-31 05:37:30 +0530

    started first script MG1.sh

---
+++ MS1.sh
@@ -0,0 +1,3 @@
+# MG1.sh - Find missing content on the text files
+
+# This bash script will help you find the missing content on the text files present on the folder specified by the user input as first parameter while executing this script.
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Magic-Bash-Scripts
+# Magic Bash Scripts
 This repository contains the code for scripts I created while playing with Bash.
+
+### Index
+
+* [MS1.sh](/MS1.sh) (WIP) - Find missing content on the text files
