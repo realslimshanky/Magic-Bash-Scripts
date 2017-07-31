49cebb616a38708ea64db82f081510def4675235
Shashank <abc@shanky.xyz>
2017-07-31 07:10:51 +0530

    Travis CI integrated and minor changes

--- MS1.sh
+++ MS1.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 # MG1.sh - Find missing content on the text files
-
 # This bash script will help you find the missing content on the text files present on the folder specified by the user input as first parameter while executing this script.
 
 echo 'Work In Progress'
